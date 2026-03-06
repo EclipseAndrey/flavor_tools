a84a372e2959a38e81ea634b214ca1cceb9073e4
Andrey Korenkov <korenkov80@inbox.ru>
2026-03-06 23:24:42 +0300

    Fix analyze

--- example/flavor_tools_example.dart
+++ example/flavor_tools_example.dart
@@ -1,5 +1,3 @@
-import 'package:flavor_tools/flavor_tools.dart';
-
 // void main() {
 //   var awesome = Awesome();
 //   print('awesome: ${awesome.isAwesome}');
--- lib/flavor_tools.dart
+++ lib/flavor_tools.dart
@@ -4,6 +4,6 @@ import 'package:flavor_tools/runner_args.dart';
 
 export 'package:flavor_tools/src/create_flavor/create_flavor.dart';
 
-flavorTools(List<String> arguments) async {
+Future<void> flavorTools(List<String> arguments) async {
   await runnerArgs(arguments);
 }
